366872295971abf4138e19f477a19e04fb41c72d
Vitor Bastos <vitorbastosbn@gmail.com>
2018-04-15 15:00:03 -0300

    -- Outro teste de configuração

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static(_dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(_dirname + '/dist/index.html'));
+})
+
+console.log('Console ouvindo!');
