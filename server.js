c89264da0410a4ec293118fb418f1a8655cb0e3d
Vitor Bastos <vitorbastosbn@gmail.com>
2018-04-15 19:30:42 -0300

    -- t

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
+//Install express server
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static(_dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(_dirname + '/dist/index.html'));
-})
-
-console.log('Console ouvindo!');
